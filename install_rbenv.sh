40a68436b6a39425aff612b40f45caed26465628
doridoridoriand <i.am.eager.for.peace@gmail.com>
2018-08-18 21:44:49 +0900

    最後にセミコロンつける方向に統一した

--- install_rbenv.sh
+++ install_rbenv.sh
@@ -11,19 +11,19 @@ if [[ $osName -eq "Darwin" ]]; then
 fi
 
 if [[ $linuxDistoribution -eq 0 ]]; then
-  sudo apt-get update
-  sudo apt-get -y install git curl g++ make
-  sudo apt-get -y install zlib1g-dev libssl-dev libreadline-dev
-  sudo apt-get -y install libyaml-dev libxml2-dev libxslt-dev
+  sudo apt-get update;
+  sudo apt-get -y install git curl g++ make;
+  sudo apt-get -y install zlib1g-dev libssl-dev libreadline-dev;
+  sudo apt-get -y install libyaml-dev libxml2-dev libxslt-dev:
 fi
 
 if [[ $linuxDistoribution -eq 1 ]]; then
-  sudo yum install -y git gcc gcc-c++ openssl-devel readline-devel
+  sudo yum install -y git gcc gcc-c++ openssl-devel readline-devel;
 fi
 
 if [[ $osName -eq "Linux" ]]; then
-  git clone git://github.com/sstephenson/rbenv.git .rbenv
-  mkdir -p ~/.rbenv/plugins
-  cd ~/.rbenv/plugins
-  git clone git://github.com/sstephenson/ruby-build.git
+  git clone git://github.com/sstephenson/rbenv.git .rbenv;
+  mkdir -p ~/.rbenv/plugins;
+  cd ~/.rbenv/plugins;
+  git clone git://github.com/sstephenson/ruby-build.git;
 fi
